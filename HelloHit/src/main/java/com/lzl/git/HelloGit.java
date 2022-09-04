ec090481352fc4f8f49bd9836e824d606c0507dd
18857524127 <897255822@qq.com>
2022-09-04 11:35:53 +0800

    lzl添加文件

---
+++ HelloHit/src/main/java/com/lzl/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.lzl.git;
+
+/**
+ * @author Lzl
+ * @create 2022-09-04 11:28
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
